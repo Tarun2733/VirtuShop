5d2d56ebfc9d1b8425fb899a25323c9565dd6265
Tarun2733 <taruntiwari.mighty4113@gmail.com>
2023-12-31 16:17:08 +0530

    Cart feature added

--- VirtuShop/shop/views.py
+++ VirtuShop/shop/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
 from .models import Product
 from math import ceil
 
+# Create your views here.
+from django.http import HttpResponse
+
 def index(request):
-    products = Product.objects.all()
-    print(products) 
-    n = len(products)
-    nSlides = n // 4 + ceil((n / 4) - (n // 4))
-    params = {'product': products,
-              'no_of_slides': nSlides,
-              'range': range(1,nSlides),}
-    return render(request, 'shop/index.html',params)
+
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+        
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
     return render(request, 'shop/about.html')
